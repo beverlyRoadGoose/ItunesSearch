00aa94502f2d87b3ae79a279463e0f8a04a9c68e
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-10-17 23:42:42 +0200

    music video search tests

---
+++ src/test/java/com/tobiadeyinka/itunessearch/search/MusicVideoSearchTests.java
@@ -0,0 +1,233 @@
+/*
+ *  Copyright 2017 Oluwatobi Adeyinka
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package com.tobiadeyinka.itunessearch.search;
+
+import com.neovisionaries.i18n.CountryCode;
+
+import com.tobiadeyinka.itunessearch.exceptions.*;
+import com.tobiadeyinka.itunessearch.common.enums.ReturnLanguage;
+import com.tobiadeyinka.itunessearch.common.enums.ItunesApiVersion;
+import com.tobiadeyinka.itunessearch.music_videos.enums.MusicVideoAttribute;
+import com.tobiadeyinka.itunessearch.music_videos.enums.MusicVideoSearchReturnType;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.logging.Logger;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for all music video search methods.
+ *
+ * Created by Tobi Adeyinka on 2017. 10. 17..
+ */
+public class MusicVideoSearchTests {
+
+    private String searchTerm = "the national";
+    private Logger logger = Logger.getLogger(MusicVideoSearchTests.class.getName());
+
+    private MusicVideoSearch search = null;
+    private JSONObject response = null;
+
+    private static final String TEST_LOG_TAG = "test: ";
+    private static final String URL_LOG_TAG = "search url: ";
+    private static final String RESPONSE_LOG_TAG = "search response: ";
+
+    @Test(expectedExceptions = MissingRequiredParameterException.class)
+    public void searchForMusicVideoWithoutSearchTerm() throws ItunesSearchException {
+        new MusicVideoSearch().execute();
+    }
+
+    @Test
+    public void searchForMusicVideoWithDefaultParameters() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new MusicVideoSearch().with(searchTerm);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForMusicVideoUsingArtistAttribute() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new MusicVideoSearch()
+                    .with(searchTerm)
+                    .inAttribute(MusicVideoAttribute.ARTIST);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForMusicVideoInSpecificStore() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new MusicVideoSearch()
+                    .with(searchTerm)
+                    .inCountry(CountryCode.NG);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForMusicVideoWithLimit() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            int limit = 5;
+            search = new MusicVideoSearch()
+                    .with(searchTerm)
+                    .withLimit(limit);
+            response = search.execute();
+
+            verifyResponseHasResults();
+            verifyResponseMatchesLimit(limit);
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForMusicVideoWithApiVersion1() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new MusicVideoSearch()
+                    .with(searchTerm)
+                    .withApiVersion(ItunesApiVersion.ONE);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForMusicVideoWithJapaneseResponse() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new MusicVideoSearch()
+                    .with(searchTerm)
+                    .withReturnLanguage(ReturnLanguage.JAPANESE);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForMusicVideoWithArtistReturnType() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new MusicVideoSearch()
+                    .with(searchTerm)
+                    .inAttribute(MusicVideoAttribute.ARTIST)
+                    .andReturn(MusicVideoSearchReturnType.MUSIC_ARTIST);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForMusicVideoWithoutExplicitContent() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new MusicVideoSearch()
+                    .with(searchTerm)
+                    .allowExplicit(false);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void comprehensiveMusicVideoSearch() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            int limit = 5;
+            search = new MusicVideoSearch()
+                    .with(searchTerm)
+                    .withLimit(limit)
+                    .inCountry(CountryCode.NG)
+                    .inAttribute(MusicVideoAttribute.ARTIST)
+                    .withReturnLanguage(ReturnLanguage.JAPANESE)
+                    .withApiVersion(ItunesApiVersion.ONE);
+            response = search.execute();
+
+            verifyResponseHasResults();
+            verifyResponseMatchesLimit(limit);
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    private void verifyResponseHasResults() {
+        assertThat(response.has("results"));
+    }
+
+    private void verifyResponseMatchesLimit(int limit) {
+        JSONArray matchingPodcastsArray = response.getJSONArray("results");
+        assertThat(matchingPodcastsArray.length())
+                .isGreaterThan(0)
+                .isLessThan(limit + 1);
+    }
+
+    private void logUrlAndResponse() {
+        String callingMethodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+
+        if (search != null && response != null) {
+            logger.info("\n" + TEST_LOG_TAG + callingMethodName + "\n"
+                    + URL_LOG_TAG + search.getSearchUrl() + "\n"
+                    + RESPONSE_LOG_TAG + response.toString() + "\n\n");
+        }
+    }
+
+    /*
+     * search and response objects are nullified at the start of
+     * each tests to prevent the values from a previous test from
+     * being used when the current test fails.
+     */
+    private void nullifySearchAndResponse() {
+        search = null;
+        response = null;
+    }
+    
+}
