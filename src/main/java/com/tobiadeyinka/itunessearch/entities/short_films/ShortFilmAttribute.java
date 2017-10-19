2a472781d67f69f7cbb196c0470445f5211ba301
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-10-19 18:23:34 +0200

    create short film attribute enum

---
+++ src/main/java/com/tobiadeyinka/itunessearch/entities/short_films/ShortFilmAttribute.java
@@ -0,0 +1,51 @@
+/*
+ *  Copyright 2017 Oluwatobi Adeyinka
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package com.tobiadeyinka.itunessearch.entities.short_films;
+
+/**
+ * Enumeration of short film attributes that can be used to narrow down searches.
+ *
+ * See attribute parameter key in <a href="https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/#searching">
+ *     Searching the iTunes Store</a> for more details.
+ *
+ * Created by Tobi Adeyinka on 2017. 10. 19..
+ */
+public enum ShortFilmAttribute {
+
+    ALL(""),
+    GENRE("genreIndex"),
+    ARTIST("artistTerm"),
+    RATING("ratingIndex"),
+    SHORT_FILM("shortFilmTerm"),
+    DESCRIPTION("descriptionTerm");
+
+    private String parameterValue;
+
+    ShortFilmAttribute(String parameterValue) {
+        this.parameterValue = parameterValue;
+    }
+
+    /**
+     *
+     * @return the value to be passed in the api request
+     */
+    public String getParameterValue() {
+        return parameterValue;
+    }
+
+}
