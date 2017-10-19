eda179f5e5386a64f23054bd79ad4e0d9167de3e
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-10-19 23:35:01 +0200

    implement short films search

--- src/main/java/com/tobiadeyinka/itunessearch/search/SearchEndpoint.java
+++ src/main/java/com/tobiadeyinka/itunessearch/search/SearchEndpoint.java
@@ -32,14 +32,80 @@ import org.json.JSONObject;
  */
 interface SearchEndpoint<T, U, V> {
 
+    /**
+     * Sets the term to search for. Required.
+     *
+     * @param searchTerm the term to search for
+     * @return the current instance of {@link T}
+     */
     T with(String searchTerm);
+
+    /**
+     * Sets the maximum number of item's to return. Default is 50.
+     *
+     * @param limit the maximum number of item's to return.
+     * @return the current instance of {@link T}
+     */
     T withLimit(int limit);
+
+    /**
+     * Sets the attribute the search term is compared with. Default is all attributes.
+     *
+     * @param attribute the attribute the search term is compared with.
+     * @return the current instance of {@link T}
+     */
     T inAttribute(U attribute);
+
+    /**
+     * Sets the iTunes store to search.
+     *
+     * @param countryCode <a href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">ISO 3166-1 alpha-2</a> country code
+     *                    for the iTunes store to search. Default is US.
+     * @return the current instance of {@link T}
+     */
     T inCountry(CountryCode countryCode);
+
+    /**
+     * Set the version of the iTunes api to use (1/2). Default is 2.
+     *
+     * @param apiVersion the version of the iTunes api to use.
+     * @return the current instance of {@link T}
+     */
     T withApiVersion(ItunesApiVersion apiVersion);
+
+    /**
+     * Sets the language results are return in. Default is english.
+     *
+     * @param returnLanguage The language result should be return in.
+     * @return the current instance of {@link T}
+     */
     T withReturnLanguage(ReturnLanguage returnLanguage);
+
+    /**
+     * allow/remove explicit content in search results. Explicit content is allowed by default.
+     *
+     * @param allowExplicit allow explicit content or not.
+     * @return the current instance of {@link T}
+     */
     T allowExplicit(boolean allowExplicit);
+
+    /**
+     * Sets the return type of the results
+     *
+     * @param returnType the type of results you want returned
+     * @return the current instance of {@link T}
+     */
     T andReturn(V returnType);
+
+    /**
+     * execute the search
+     *
+     * @return A {@link org.json.JSONObject} object containing the results.
+     * @throws MissingRequiredParameterException if the search term is not set.
+     * @throws InvalidParameterException if any of the set parameters are invalid.
+     * @throws SearchURLConstructionFailure if there is an error during url construction.
+     * @throws NetworkCommunicationException if any issues occur while communicating with the iTunes api.
+     */
     JSONObject execute() throws ItunesSearchException;
 
 }
