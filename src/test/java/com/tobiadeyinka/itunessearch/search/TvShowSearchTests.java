add048203994d1315a1d2ca24ae6f47fa451503a
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-10-25 14:55:14 +0200

    tv show search tests

--- src/test/java/com/tobiadeyinka/itunessearch/search/TvShowSearchTests.java
+++ src/test/java/com/tobiadeyinka/itunessearch/search/TvShowSearchTests.java
@@ -17,8 +17,237 @@
 
 package com.tobiadeyinka.itunessearch.search;
 
+import com.neovisionaries.i18n.CountryCode;
+
+import com.tobiadeyinka.itunessearch.entities.ReturnLanguage;
+import com.tobiadeyinka.itunessearch.entities.ItunesApiVersion;
+
+import com.tobiadeyinka.itunessearch.entities.tv_shows.TvShowAttribute;
+import com.tobiadeyinka.itunessearch.entities.tv_shows.TvShowSearchReturnType;
+
+import com.tobiadeyinka.itunessearch.exceptions.ItunesSearchException;
+import com.tobiadeyinka.itunessearch.exceptions.MissingRequiredParameterException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.logging.Logger;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
+ * Tests for all tv show search methods.
+ *
  * Created by Tobi Adeyinka on 2017. 10. 23..
  */
 public class TvShowSearchTests {
+
+    private String searchTerm = "the";
+    private Logger logger = Logger.getLogger(TvShowSearchTests.class.getName());
+
+    private TvShowSearch search = null;
+    private JSONObject response = null;
+
+    private static final String TEST_LOG_TAG = "test: ";
+    private static final String URL_LOG_TAG = "search url: ";
+    private static final String RESPONSE_LOG_TAG = "search response: ";
+
+    @Test(expectedExceptions = MissingRequiredParameterException.class)
+    public void searchForTvShowWithoutSearchTerm() throws ItunesSearchException {
+        new TvShowSearch().execute();
+    }
+
+    @Test
+    public void searchForTvShowWithDefaultParameters() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new TvShowSearch().with(searchTerm);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForTvShowUsingEpisodeAttribute() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new TvShowSearch()
+                    .with(searchTerm)
+                    .inAttribute(TvShowAttribute.TV_EPISODE);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForTvShowInSpecificStore() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new TvShowSearch()
+                    .with(searchTerm)
+                    .inCountry(CountryCode.NG);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForTvShowWithLimit() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            int limit = 5;
+            search = new TvShowSearch()
+                    .with(searchTerm)
+                    .withLimit(limit);
+            response = search.execute();
+
+            verifyResponseHasResults();
+            verifyResponseMatchesLimit(limit);
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForTvShowWithApiVersion1() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new TvShowSearch()
+                    .with(searchTerm)
+                    .withApiVersion(ItunesApiVersion.ONE);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForTvShowWithJapaneseResponse() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new TvShowSearch()
+                    .with(searchTerm)
+                    .withReturnLanguage(ReturnLanguage.JAPANESE);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForTvShowWithEpisodeReturnType() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new TvShowSearch()
+                    .with(searchTerm)
+                    .inAttribute(TvShowAttribute.TV_EPISODE)
+                    .andReturn(TvShowSearchReturnType.EPISODE);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void searchForTvShowWithoutExplicitContent() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            search = new TvShowSearch()
+                    .with(searchTerm)
+                    .allowExplicit(false);
+            response = search.execute();
+            verifyResponseHasResults();
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    @Test
+    public void comprehensiveTvShowSearch() throws ItunesSearchException {
+        nullifySearchAndResponse();
+
+        try {
+            int limit = 5;
+            search = new TvShowSearch()
+                    .with(searchTerm)
+                    .withLimit(limit)
+                    .inCountry(CountryCode.US)
+                    .inAttribute(TvShowAttribute.TV_EPISODE)
+                    .withReturnLanguage(ReturnLanguage.JAPANESE)
+                    .withApiVersion(ItunesApiVersion.ONE);
+            response = search.execute();
+
+            verifyResponseHasResults();
+            verifyResponseMatchesLimit(limit);
+        } finally {
+            logUrlAndResponse();
+        }
+    }
+
+    private void verifyResponseHasResults() {
+        assertThat(response.has("results"));
+    }
+
+    private void verifyResponseMatchesLimit(int limit) {
+        JSONArray matchingPodcastsArray = response.getJSONArray("results");
+        assertThat(matchingPodcastsArray.length())
+                .isGreaterThanOrEqualTo(0)
+                .isLessThan(limit + 1);
+    }
+
+    private void logUrlAndResponse() {
+        String callingMethodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+
+        if (search != null && response != null) {
+            logger.info("\n" + TEST_LOG_TAG + callingMethodName + "\n"
+                    + URL_LOG_TAG + search.getSearchUrl() + "\n"
+                    + RESPONSE_LOG_TAG + response.toString() + "\n\n");
+        }
+    }
+
+    /*
+     * search and response objects are nullified at the start of
+     * each tests to prevent the values from a previous test from
+     * being used when the current test fails.
+     */
+    private void nullifySearchAndResponse() {
+        search = null;
+        response = null;
+    }
+
+    /*
+     * The itunes api limits call per minute, so sleep for a minute before the next
+     * set of tests start, to avoid failures.
+     */
+    @AfterClass
+    private void sleepForAMinute() {
+        logger.info("sleeping for 1 minute");
+
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
