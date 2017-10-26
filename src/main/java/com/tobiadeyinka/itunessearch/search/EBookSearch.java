28568699451a13d13e0e2b2298507a08684570fc
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-10-26 18:41:54 +0200

    minify ebooks search

--- src/main/java/com/tobiadeyinka/itunessearch/search/EBookSearch.java
+++ src/main/java/com/tobiadeyinka/itunessearch/search/EBookSearch.java
@@ -17,20 +17,9 @@
 
 package com.tobiadeyinka.itunessearch.search;
 
-import com.neovisionaries.i18n.CountryCode;
-
 import com.tobiadeyinka.itunessearch.entities.ItunesMedia;
-import com.tobiadeyinka.itunessearch.entities.ReturnLanguage;
-import com.tobiadeyinka.itunessearch.entities.ItunesApiVersion;
 import com.tobiadeyinka.itunessearch.entities.ebooks.EBookAttribute;
 import com.tobiadeyinka.itunessearch.entities.ebooks.EBookSearchReturnType;
-
-import com.tobiadeyinka.itunessearch.exceptions.*;
-
-import org.json.JSONObject;
-
-import java.net.URL;
-import java.net.MalformedURLException;
 
 /**
  * EBook search API endpoint.
@@ -40,12 +29,7 @@ import java.net.MalformedURLException;
  *
  * Created by Tobi Adeyinka on 2017. 10. 26..
  */
-public class EBookSearch extends Search implements SearchEndpoint<EBookSearch, EBookAttribute, EBookSearchReturnType> {
-
-    /**
-     * The term to search for.
-     */
-    private String searchTerm;
+public class EBookSearch extends Search<EBookSearch> {
 
     /**
      * The media type to search for. In this case eBooks.
@@ -53,35 +37,9 @@ public class EBookSearch extends Search implements SearchEndpoint<EBookSearch, E
     private final ItunesMedia media = ItunesMedia.EBOOK;
 
     /**
-     * The version of the iTunes api to use (1/2). Default is 2.
-     */
-    private int apiVersion = 2;
-
-    /**
-     * The maximum number of item's to return. Default is 50.
-     */
-    private int limit = 50;
-
-    /**
-     * allow/exclude explicit content in search results. Explicit content is allowed by default.
-     */
-    private boolean allowExplicit = true;
-
-    /**
      * The eBook attribute the search term is compared with. Default is all attributes.
      */
     private EBookAttribute attribute = EBookAttribute.ALL;
-
-    /**
-     *  <a href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">ISO 3166-1 alpha-2</a> country code for the
-     *  iTunes store to search. Default is US.
-     */
-    private CountryCode countryCode = CountryCode.US;
-
-    /**
-     * The language to return the result in (only english/japanese). Default is english.
-     */
-    private ReturnLanguage returnLanguage = ReturnLanguage.ENGLISH;
 
     /**
      * The type of results returned
@@ -89,92 +47,13 @@ public class EBookSearch extends Search implements SearchEndpoint<EBookSearch, E
     private EBookSearchReturnType returnType = EBookSearchReturnType.DEFAULT;
 
     /**
-     * URL used to search the iTunes store, generated using all the variables of the instance
-     */
-    private URL searchUrl;
-
-    /**
-     * Sets the term to search for. Required.
-     *
-     * @param searchTerm the term to search for
-     * @return the current instance of {@link EBookSearch}
-     */
-    @Override
-    public EBookSearch with(String searchTerm) {
-        this.searchTerm = searchTerm;
-        return this;
-    }
-
-    /**
-     * Sets the maximum number of item's to return. Default is 50.
-     *
-     * @param limit the maximum number of item's to return.
-     * @return the current instance of {@link EBookSearch}
-     */
-    @Override
-    public EBookSearch withLimit(int limit) {
-        this.limit = limit;
-        return this;
-    }
-
-    /**
      * Sets the eBook attribute the search term is compared with. Default is all attributes.
      *
      * @param attribute the eBook attribute the {@link #searchTerm} is compared with.
      * @return the current instance of {@link EBookSearch}
      */
-    @Override
     public EBookSearch inAttribute(EBookAttribute attribute) {
         this.attribute = attribute;
-        return this;
-    }
-
-    /**
-     * Sets the iTunes store to search.
-     *
-     * @param countryCode <a href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">ISO 3166-1 alpha-2</a> country code
-     *                    for the iTunes store to search. Default is US.
-     * @return the current instance of {@link EBookSearch}
-     */
-    @Override
-    public EBookSearch inCountry(CountryCode countryCode) {
-        this.countryCode = countryCode;
-        return this;
-    }
-
-    /**
-     * Set the version of the iTunes api to use (1/2). Default is 2.
-     *
-     * @param apiVersion the version of the iTunes api to use.
-     * @return the current instance of {@link EBookSearch}
-     */
-    @Override
-    public EBookSearch withApiVersion(ItunesApiVersion apiVersion) {
-        this.apiVersion = apiVersion.getVersionNumber();
-        return this;
-    }
-
-    /**
-     * Sets the language results are return in. Default is english.
-     *
-     * @param returnLanguage The language result should be return in.
-     * @return the current instance of {@link EBookSearch}
-     */
-    @Override
-    public EBookSearch withReturnLanguage(ReturnLanguage returnLanguage) {
-        this.returnLanguage = returnLanguage;
-        return this;
-    }
-
-    /**
-     * allow/remove explicit content in search results. Explicit content is allowed by default.
-     *
-     * @param allowExplicit allow explicit content or not.
-     * @return the current instance of {@link EBookSearch}
-     */
-    @Override
-    public EBookSearch allowExplicit(boolean allowExplicit) {
-        this.allowExplicit = allowExplicit;
         return this;
     }
 
@@ -184,37 +63,12 @@ public class EBookSearch extends Search implements SearchEndpoint<EBookSearch, E
      * @param returnType the type of results you want returned
      * @return the current instance of {@link EBookSearch}
      */
-    @Override
     public EBookSearch andReturn(EBookSearchReturnType returnType) {
         this.returnType = returnType;
         return this;
     }
 
-    /**
-     * execute the search
-     *
-     * @return A {@link org.json.JSONObject} object containing the results.
-     * @throws MissingRequiredParameterException if the search term is not set.
-     * @throws InvalidParameterException if any of the set parameters are invalid.
-     * @throws SearchURLConstructionFailure if there is an error during url construction.
-     * @throws NetworkCommunicationException if any issues occur while communicating with the iTunes api.
-     */
-    public JSONObject execute() throws ItunesSearchException {
-        runPreExecutionChecks();
-        String urlString = constructUrlString();
-        URL url = createUrlObject(urlString);
-        searchUrl = url;
-        return new SearchManager().executeSearch(url);
-    }
-
-    private URL createUrlObject(String urlString) throws ItunesSearchException {
-        try {
-            return new URL(urlString);
-        } catch (MalformedURLException e) {
-            throw new SearchURLConstructionFailure("Error during search url construction: " + e.getMessage());
-        }
-    }
-
+    @Override
     String constructUrlString() {
         String urlString = "https://itunes.apple.com/search?";
         urlString += "term=" + searchTerm;
@@ -232,42 +86,10 @@ public class EBookSearch extends Search implements SearchEndpoint<EBookSearch, E
 
     /**
      *
-     * @return the term to be searched for
-     */
-    public String getSearchTerm() {
-        return searchTerm;
-    }
-
-    /**
-     *
      * @return the set media type being searched for
      */
     public ItunesMedia getMedia() {
         return media;
-    }
-
-    /**
-     *
-     * @return the api version to use
-     */
-    public int getApiVersion() {
-        return apiVersion;
-    }
-
-    /**
-     *
-     * @return the maximum number of results
-     */
-    public int getLimit() {
-        return limit;
-    }
-
-    /**
-     *
-     * @return the explicit data setting
-     */
-    public boolean explicitAllowed() {
-        return allowExplicit;
     }
 
     /**
@@ -280,34 +102,10 @@ public class EBookSearch extends Search implements SearchEndpoint<EBookSearch, E
 
     /**
      *
-     * @return the iTunes store being searched
-     */
-    public CountryCode getCountryCode() {
-        return countryCode;
-    }
-
-    /**
-     *
-     * @return the language the result should be returned in
-     */
-    public ReturnLanguage getReturnLanguage() {
-        return returnLanguage;
-    }
-
-    /**
-     *
      * @return the type of data to return
      */
     public EBookSearchReturnType getReturnType() {
         return returnType;
-    }
-
-    /**
-     *
-     * @return the url used to search the iTunes store.
-     */
-    public URL getSearchUrl() {
-        return searchUrl;
     }
 
 }
