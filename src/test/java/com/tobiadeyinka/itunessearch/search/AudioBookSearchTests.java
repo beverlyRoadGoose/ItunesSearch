eb6c71148755aa5964f0bab77d6d6934ecde4358
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-10-27 13:59:23 +0200

    minify audio book search tests

--- src/test/java/com/tobiadeyinka/itunessearch/search/AudioBookSearchTests.java
+++ src/test/java/com/tobiadeyinka/itunessearch/search/AudioBookSearchTests.java
@@ -27,35 +27,19 @@ import com.tobiadeyinka.itunessearch.entities.audio_books.AudioBookSearchReturnT
 import com.tobiadeyinka.itunessearch.exceptions.ItunesSearchException;
 import com.tobiadeyinka.itunessearch.exceptions.MissingRequiredParameterException;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import org.testng.annotations.Test;
-import org.testng.annotations.AfterClass;
-
-import java.util.logging.Logger;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Tests for all audio book search methods.
  *
  * Created by Tobi Adeyinka on 2017. 10. 18..
  */
-public class AudioBookSearchTests {
+public class AudioBookSearchTests extends BaseSearchTest {
 
     /*
      * used a common word on purpose
      */
     private String searchTerm = "the";
-    private Logger logger = Logger.getLogger(AudioBookSearchTests.class.getName());
-
-    private AudioBookSearch search = null;
-    private JSONObject response = null;
-
-    private static final String TEST_LOG_TAG = "test: ";
-    private static final String URL_LOG_TAG = "search url: ";
-    private static final String RESPONSE_LOG_TAG = "search response: ";
 
     @Test(expectedExceptions = MissingRequiredParameterException.class)
     public void searchForAudioBookWithoutSearchTerm() throws ItunesSearchException {
@@ -203,52 +187,6 @@ public class AudioBookSearchTests {
             verifyResponseMatchesLimit(limit);
         } finally {
             logUrlAndResponse();
-        }
-    }
-
-    private void verifyResponseHasResults() {
-        assertThat(response.has("results"));
-    }
-
-    private void verifyResponseMatchesLimit(int limit) {
-        JSONArray matchingPodcastsArray = response.getJSONArray("results");
-        assertThat(matchingPodcastsArray.length())
-                .isGreaterThanOrEqualTo(0)
-                .isLessThan(limit + 1);
-    }
-
-    private void logUrlAndResponse() {
-        String callingMethodName = Thread.currentThread().getStackTrace()[2].getMethodName();
-
-        if (search != null && response != null) {
-            logger.info("\n" + TEST_LOG_TAG + callingMethodName + "\n"
-                    + URL_LOG_TAG + search.getSearchUrl() + "\n"
-                    + RESPONSE_LOG_TAG + response.toString() + "\n\n");
-        }
-    }
-
-    /*
-     * search and response objects are nullified at the start of
-     * each tests to prevent the values from a previous test from
-     * being used when the current test fails.
-     */
-    private void nullifySearchAndResponse() {
-        search = null;
-        response = null;
-    }
-
-    /*
-     * The itunes api limits call per minute, so sleep for a minute before the next
-     * set of tests start, to avoid failures.
-     */
-    @AfterClass
-    private void sleepForAMinute() {
-        logger.info("sleeping for 1 minute");
-
-        try {
-            Thread.sleep(60000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
     
---
+++ src/test/java/com/tobiadeyinka/itunessearch/search/BaseSearchTest.java
@@ -0,0 +1,91 @@
+/*
+ *  Copyright 2017 Oluwatobi Adeyinka
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package com.tobiadeyinka.itunessearch.search;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import org.testng.annotations.AfterClass;
+
+import java.util.logging.Logger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Base class for all tests.
+ *
+ * Created by Tobi Adeyinka on 2017. 10. 27..
+ */
+public class BaseSearchTest {
+
+    protected Logger logger = Logger.getLogger(BaseSearchTest.class.getName());
+
+    protected static final String TEST_LOG_TAG = "test: ";
+    protected static final String URL_LOG_TAG = "search url: ";
+    protected static final String RESPONSE_LOG_TAG = "search response: ";
+
+    protected Search search = null;
+    protected JSONObject response = null;
+
+    protected void verifyResponseHasResults() {
+        assertThat(response.has("results"));
+    }
+
+    protected void verifyResponseMatchesLimit(int limit) {
+        JSONArray matchingPodcastsArray = response.getJSONArray("results");
+        assertThat(matchingPodcastsArray.length())
+                .isGreaterThanOrEqualTo(0)
+                .isLessThan(limit + 1);
+    }
+
+    protected void logUrlAndResponse() {
+        String callingMethodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+
+        if (search != null && response != null) {
+            logger.info("\n" + TEST_LOG_TAG + callingMethodName + "\n"
+                    + URL_LOG_TAG + search.getSearchUrl() + "\n"
+                    + RESPONSE_LOG_TAG + response.toString() + "\n\n");
+        }
+    }
+
+    /*
+     * search and response objects are nullified at the start of
+     * each tests to prevent the values from a previous test from
+     * being used when the current test fails.
+     */
+    protected void nullifySearchAndResponse() {
+        search = null;
+        response = null;
+    }
+
+    /*
+     * The itunes api limits call per minute, so sleep for a minute before the next
+     * set of tests start, to avoid failures.
+     */
+    @AfterClass
+    protected void sleepForAMinute() {
+        logger.info("sleeping for 1 minute");
+
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
