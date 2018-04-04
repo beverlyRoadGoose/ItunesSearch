52be436610b95207fe38279eb3d9cf30f9210ccb
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2018-04-05 00:20:21 +0200

    minor refactoring

--- src/test/java/com/tobiadeyinka/itunessearch/search/MusicSearchTests.java
+++ src/test/java/com/tobiadeyinka/itunessearch/search/MusicSearchTests.java
@@ -19,11 +19,7 @@ package com.tobiadeyinka.itunessearch.search;
 
 import com.neovisionaries.i18n.CountryCode;
 
-import com.tobiadeyinka.itunessearch.entities.ReturnLanguage;
-import com.tobiadeyinka.itunessearch.entities.ItunesApiVersion;
-import com.tobiadeyinka.itunessearch.entities.MusicAttribute;
-import com.tobiadeyinka.itunessearch.entities.MusicSearchReturnType;
-
+import com.tobiadeyinka.itunessearch.entities.*;
 import com.tobiadeyinka.itunessearch.exceptions.*;
 
 import org.testng.annotations.Test;
